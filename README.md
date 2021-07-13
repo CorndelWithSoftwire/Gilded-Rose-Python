d72b6b7dae9b6394bc160c3f515d2d350f89c2f7
Hugh Emerson <Hugh.Emerson@softwire.com>
2021-07-13 13:40:14 +0100

    A possible rewrite of gilded_rose.py Add some thoughts to the README.md

--- README.md
+++ README.md
@@ -11,3 +11,13 @@ python golden_master_test.py
 ```
 
 This should output that it ran 1 test, and the overall result was 'OK'.
+
+## Notes on the "solution"
+
+This is just a possible refactor; there are definitely aspects that can be debated.
+
+But some thoughts:
+- The main aim is to make the code clearly map to business logic and generally be a bit easier to read
+- A result of that is it's easier to modify the functionality for different item types or add new types
+- It is far from a perfect solution, but mostly because of the limitation of only modifying `GildedRose`. In reality you might consider writing subclasses of Item for the different types, or at least put some properties/methods on the Item class.
+- The main point of the exercise was not to reach some goal but the process of small incremental improvements, frequently running the test, and frequently making small, self-contained commits)
--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -1,34 +1,54 @@
 class GildedRose(object):
+    # These constants and a lot of the functions should live in Item, but I have stuck to the rules of the exercise
+    MAX_QUALITY = 50
+    MIN_QUALITY = 0
+    BRIE_NAME = "Aged Brie"
+    BACKSTAGE_PASSES_NAME = "Backstage passes to a TAFKAL80ETC concert"
+    SULFURAS_NAME = "Sulfuras, Hand of Ragnaros"
 
     def __init__(self, items):
         self.items = items
 
+    # I have left the "interface" of the class unchanged
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            if self._is_legendary(item):
+                continue
+
+            self._update_item_quality(item)
+            item.sell_in -= 1
+        
+    def _update_item_quality(self, item):
+        if item.name == GildedRose.BACKSTAGE_PASSES_NAME:
+            self._update_backstage_passes_quality(item)
+        elif item.name == GildedRose.BRIE_NAME:
+            self._update_normal_item_quality(item, multiplier=-1)
+        # new feature (test will need updating)
+        # elif self._is_conjured_item(item):
+        #     self._update_normal_item_quality(item, multiplier=2)
+        else:
+            self._update_normal_item_quality(item)
+
+    def _update_backstage_passes_quality(self, passes):
+        if passes.sell_in < 1:
+            passes.quality = 0
+        elif passes.sell_in < 6:
+            self._change_quality_within_limits(passes, 3)
+        elif passes.sell_in < 11:
+            self._change_quality_within_limits(passes, 2)
+        else:
+            self._change_quality_within_limits(passes, 1)
+
+    def _update_normal_item_quality(self, item, multiplier=1):
+        normal_change = -1 if item.sell_in > 0 else -2
+        self._change_quality_within_limits(item, normal_change * multiplier)
+
+    def _change_quality_within_limits(self, item, change):
+        new_quality = item.quality + change
+        item.quality = max(GildedRose.MIN_QUALITY, min(GildedRose.MAX_QUALITY, new_quality))
+
+    def _is_legendary(self, item):
+        return item.name == GildedRose.SULFURAS_NAME
+
+    def _is_conjured_item(self, item):
+        return "conjured" in item.name.lower() # Actual business logic was not specified
